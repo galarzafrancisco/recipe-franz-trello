288cf20d7ecb131980625d4e1cfef520b4bbcfcb
Thom van Hal <Thomvh@users.noreply.github.com>
2017-10-25 00:47:13 +0200

    Initial commit

---
+++ trello/webview.js
@@ -0,0 +1,9 @@
+module.exports = (Franz) => {
+  const getMessages = function getMessages() {
+    const notifications = document.querySelectorAll('.new-notifications');
+
+    Franz.setBadge(0, (notifications.length >= 1) ? 1 : 0);
+  };
+
+  Franz.loop(getMessages);
+};
